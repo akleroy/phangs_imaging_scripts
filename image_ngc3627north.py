46246787fb9d8c6bb5d09b0c4e448e8a63a89e84
akleroy <leroy.42@tycho.astronomy.osu.edu>
2017-06-09 12:32:44 -0400

    ...

--- image_ngc3627north.py
+++ image_ngc3627north.py
@@ -11,7 +11,7 @@ tag = '956'
 # overall center: phase_center = 'J2000 11h20m14.9s +12d59m30'
 phase_center = 'J2000 11h20m14.9s +12d59m59'
 source_vel_kms = 727
-vwidth_kms = 500
+vwidth_kms = 700
 
 calibrated_files = {'12m':'../../2015.1.00956.S/science_goal.uid___A001_X2fb_X285/group.uid___A001_X2fb_X286/member.uid___A001_X2fb_X287/calibrated/uid___A002_Xaf5c32_X6d2.ms.split.cal',
                     '7m':'../../2015.1.00956.S/science_goal.uid___A001_X2fb_X285/group.uid___A001_X2fb_X286/member.uid___A001_X2fb_X289/calibrated/calibrated_final.ms'}
@@ -25,10 +25,10 @@ clean_mask_file = '../clean_masks/ngc3627_co21_widemask.fits'
 execfile('../scripts/line_list.py')
 
 # Extract data
-script_copy = False
-script_extract_co21 = False
-script_extract_c18o21 = False
-script_extract_continuum = False
+script_copy = True
+script_extract_co21 = True
+script_extract_c18o21 = True
+script_extract_continuum = True
 
 # Image data
 script_image_cube = True
