d5dcce406171f9d912c53076bb047f0d5a1090a2
akleroy <leroy.42@tycho.astronomy.osu.edu>
2017-11-01 14:15:57 -0400

    ...

--- phangsPipeline.py
+++ phangsPipeline.py
@@ -150,6 +150,40 @@ def read_mosaic_key(fname='../scripts/mosaic_definitions.txt'):
     
     return mosaic_key
 
+def read_override_image_params(
+    fname='../scripts/override_image_params.txt'):
+    """
+    Read hand set overrides for cell and image sizes.
+    """
+
+    infile = open(fname, 'r')
+
+    override_dict = {}
+    while True:
+        line = infile.readline()    
+        if len(line) == 0:
+            break
+        if line[0] == '#':
+            continue
+        words = line.split()
+        if len(words) < 3:
+            continue
+        vis_override = words[0]
+        param_override = words[1]
+        value_override = words[2]
+        if override_dict.has_key(vis_override) == False:
+            override_dict[vis_override] = {}
+        override_dict[vis_override][param_override] = value_override
+            
+    infile.close()
+
+    return override_dict
+
+def read_imaging_key(fname='../scripts/imaging_key.txt'):
+    """
+    """
+    pass
+
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
 # Routines to move data around.
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
@@ -1167,7 +1201,82 @@ def extract_continuum_for_galaxy(
 
     return
 
+# &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
+# Routines to characterize measurement sets
+# &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
+
+def estimate_cell_and_imsize(
+    in_file=None,    
+    oversamp=5
+    ):
+    """
+    Pick a cell and image size for a measurement set. Requests an
+    oversampling factor, which is by default 5. Will pick a good size
+    for the FFT and will try to pick a round number for the cell size.
+    """
+
+    if os.path.isdir(in_file) == False:
+        print "File not found."
+        return
+
+    override_dict = read_override_image_params()
     
+    valid_sizes = []
+    for ii in range(10):
+        for kk in range(3):
+            for jj in range(3):
+                valid_sizes.append(2**(ii+1)*5**(jj)*3**(kk))
+    valid_sizes.sort()
+    valid_sizes = np.array(valid_sizes)
+
+    # Cell size implied by baseline distribution from analysis
+    # utilities.
+
+    au_cellsize, au_imsize, au_centralField = \
+        au.pickCellSize(in_file, imsize=True, npix=oversamp)
+    xextent = au_cellsize*au_imsize[0]*1.2
+    yextent = au_cellsize*au_imsize[1]*1.2
+
+    # Make the cell size a nice round number
+
+    if au_cellsize < 0.1:
+        cell_size = au_cellsize
+    if au_cellsize >= 0.1 and au_cellsize < 0.5:
+        cell_size = np.floor(au_cellsize/0.05)*0.05
+    if au_cellsize >= 0.5 and au_cellsize < 1.0:
+        cell_size = np.floor(au_cellsize/0.1)*0.1
+    if au_cellsize >= 1.0 and au_cellsize < 2.0:
+        cell_size = np.floor(au_cellsize/0.25)*0.25
+    if au_cellsize >= 2.0 and au_cellsize < 5.0:
+        cell_size = np.floor(au_cellsize/0.5)*0.5
+    if au_cellsize >= 5.0:
+        cell_size = np.floor(au_cellsize/1.0)*0.5
+
+    # Now make the image size a good number for the FFT
+
+    need_cells_x = xextent / cell_size
+    need_cells_y = yextent / cell_size
+
+    cells_x = np.min(valid_sizes[valid_sizes > need_cells_x])
+    cells_y = np.min(valid_sizes[valid_sizes > need_cells_y])
+
+    image_size = [int(cells_x), int(cells_y)]
+    cell_size_string = str(cell_size)+'arcsec'
+
+    x_size_string = str(image_size[0])
+    y_size_string = str(image_size[1])
+
+    # Check for overrides
+    if override_dict.has_key(in_file):
+        if override_dict.has_key('cell_size'):
+            cell_size_string = override_dict[this_vis]['cell_size']
+        if override_dict.has_key('x_size'):
+            x_size_string = override_dict[this_vis]['x_size']
+        if override_dict.has_key('y_size'):
+            y_size_string = override_dict[this_vis]['y_size']
+
+    return cell_size_string, x_size_string, y_size_string
+
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
 # Routines to image the data
 # &%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
