af52e566c19513abc79a559bd3f0719a8010e81e
akleroy <leroy.42@tycho.astronomy.osu.edu>
2017-11-13 13:36:44 -0500

    More documentation

--- image_data.py
+++ image_data.py
@@ -1,4 +1,17 @@
-# This script carries out the imaging.
+# This script carries out the imaging. It makes a dirty image, reads
+# and aligns the user-supplied clean mask, carries out multiscale
+# imaging down to S/N ~ 4, builds a mask of bright emission, carries
+# out a single scale clean down to low S/N within this mask, and then
+# exports the output to FITS.
+
+# Right now it should work well for CO21 and C18O21. Imaging of the
+# two-d products is still TBD.
+
+# Edit the "Control Flow" section to use the script.
+
+# WARNING! Right now a bug prevents the script from stopping and
+# starting effectively, so it needs to be run end-to-end to work. This
+# shouldn't be a big problem for the 7m data.
 
 import os
 import phangsPipeline as pp
@@ -8,29 +21,67 @@ import analysisUtils as au
 # Control Flow
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# ... process only these galaxies
-only = []
+# ... a text list. The script will process only these galaxies.
+
+only = ['ngc2283'] 
 
 # ... skip these galaxies
-skip = \
-    ['ic5332',
-     'ngc0628',
-     'ngc1087',
-     'ngc1300',
-     'ngc1317',
-     ]
+
+skip = []
+
 # ... set as '12m', '7m', or '12m+7m' to process only data from that
 # array. Leave it as None to process all data.
+
 just_array = ['7m']
 
 # ... set as the products to be handled. Valid choices for the basic
-# PHANGS data are 'co21', 'c18o21', 'cont', 'co21_chan0',
-# 'c18o21_chan0'
+# PHANGS data are 'co21', 'c18o21', 'cont', 'co21_chan0', and
+# 'c18o21_chan0'. Note that right now cont and chan0 are not tested.
+
 just_product = ['co21']
 
 # ... set these variables to indicate what steps of the script should
-# be performed
+# be performed. The steps do:
+
+# make_dirty_image - make a niter=0 image cube. Useful for checking
+# mosaic parameters and that sort of thing. Used as a template for the
+# clean mask, so this needs to be done first.
+
+# revert_to_dirty - (DOES NOT WORK RIGHT NOW) reset the whole process
+# so that the cube is now the dirty cube. Unfortunately there's some
+# 'memory' related to the clean call in CASA that is causing a bug so
+# that things don't cleanly resume from here.
+
+# read_in_clean_mask - if a clean mask is found in the ../clean_masks/
+# directory, read it in and align it to the astrometric grid of the
+# dirty image. This is now the .mask file and will be used in future
+# imaging.
+
+# run_multiscale_clean - run a multiscale clean using a set of scales
+# selected for that array combination. By default, clean down to a
+# signal to noise of 4 in the residuals or stop when successive
+# iterations of clean change the flux in the model by less than 2%.
+
+# revert_to_multiscale - (DOES NOT WORK RIGHT NOW) reset the process
+# to just after the multiscale clean. See revert_to_dirty above.
+
+# make_singlescale_mask - construct a signal-to-noise based mask for
+# use in single scale clean. Uses the .image to do this, so that
+# bright regions in the image so far get more cleaning. This maks is
+# joined with the clean mask, so no regions are included outside the
+# clean mask.
+
+# run_singlescale_clean - run a single scale hogbom clean inside the
+# bright regions defined above. Clean with a very low (S/N ~ 1)
+# threshold and stop based on convergence in flux between successive
+# model images.
+
+# export_to_fits - export the dirt, multiscale, and final images and
+# associated products to FITS files for subsequent processing.
  
+# Right now I recommend to not change the flow, and to rerun imaging
+# from beginning to end when using the script.
+
 make_dirty_image=True
 revert_to_dirty=False
 read_in_clean_mask=True
--- stage_imaging.py
+++ stage_imaging.py
@@ -1,6 +1,8 @@
 # This script stages the imaging. It copies the calibrated data into
-# the imaging directories, runs any galaxy-specific processing, and
+# the imaging directories, regrids to the desired velocity axis, and
 # then extracts line and continuum data sets ready for imaging.
+
+# Edit the "Control Flow" section to use the script.
 
 import os
 import phangsPipeline as pp
@@ -10,21 +12,43 @@ import analysisUtils as au
 # Control Flow
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# ... process only these galaxies
-only = ['ngc0685', 'ngc1097_1', 'ngc1511', 
-        'ngc1792_1','ngc1809','ngc2090',
-        'ngc2283', 'ngc2775', 'ngc5134',
-        'ic5237','ngc7456']
+# ... a text list. The script will process only these galaxies.
 
-# ... skip these galaxies
+only = ['ngc2283']
+
+# ... skip these galaxies.
+
 skip = []
 
 # ... set this to '12m' or '7m' to stage data only for those
-# arrays. Leave it as None to process all data.
+# arrays. Leave it as None to process all data. If both 12m and 7m
+# data are processed, then the script will also create 12m+7m data. So
+# you need to rerun the staging when both data sets arrive.
+
 just_array = '7m'
 
 # ... set these variables to indicate what steps of the script should
-# be performed
+# carry out. The steps do:
+
+# do_copy - copy the data from the calibrated data directory to the
+# working directory. Will create the working directory for this galaxy
+# if it doesn't exist already. Uses "ms_file_key.txt" and maps
+# multi-part galaxies to directories using "dir_key.txt"
+
+# do_custom_scripts - NOT IMPLEMENTED YET. Will run custom processing
+# for each galaxy. For example, additional flagging and uv continuum
+# subtraction.
+
+# do_extract_lines - extract lines from the measurement set and regrid
+# them onto our working grid. The line set is assumed to be 12co21 and
+# c18o21 for PHANGS. The velocity grid is defined in
+# "mosaic_definitions.txt" and this step should be rerun after
+# changing that grid. Also makes "channel 0" (line integrated) data
+# sets.
+
+# do_extract_cont - extract a single-channel continuum data set from
+# the measurement set, first flagging lines. The velocity windows used
+# for flagging lines is set in "mosaic_definitions.txt"
 
 do_copy = True
 do_custom_scripts = True
