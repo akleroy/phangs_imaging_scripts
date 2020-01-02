2148e7c14bff8800cd146bc81911c66a112bfa5a
akleroy <leroy.42@maury.astronomy.osu.edu>
2020-01-02 14:55:30 -0500

    More version 2 revision.

--- test_postprocess.py
+++ test_postprocess.py
@@ -1,12 +1,24 @@
+# Imports
 from phangsPipeline import keyHandler as kh
 from phangsPipeline import postprocessHandler as pph
 
+# Instantiate handlers
 this_kh = kh.KeyHandler()
 this_pph = pph.PostProcessHandler(key_handler=this_kh, dry_run=True)
 
+# Set which data to process
 this_pph.set_interf_configs(only=['7m'])
 this_pph.set_line_products(only=['co21'])
 this_pph.set_no_cont(True)
-this_pph.set_targets(only=['ngc3521'])
+this_pph.set_targets(only=['ngc3521_1'])
 
+# Run each of the steps individually
 this_pph.stage_interferometer_data()
+this_pph.primary_beam_correct()
+this_pph.convolve_to_round_beam()
+this_pph.stage_singledish_data()
+this_pph.feather()
+this_pph.compress()
+this_pph.convert()
+this_pph.export()
+
