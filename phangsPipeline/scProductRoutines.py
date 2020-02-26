4e598ce9207705e56da83e607f6a4f9b02df3ea6
Erik Rosolowsky <rosolowsky@ualberta.ca>
2020-02-25 23:52:22 -0700

    WIP: nightly checkpoint

--- phangsPipeline/scProductRoutines.py
+++ phangsPipeline/scProductRoutines.py
@@ -148,7 +148,8 @@ def write_moment1(cube,
 
         mom1err = np.empty(mom1.shape)
         mom1err.fill(np.nan)
-
+        # Ensure the same mask applied to both.
+        rms = rms.with_mask(cube._mask, inherit_mask=False)
         for x, y, slc in cube._iter_rays(0):
             mask = np.squeeze(cube._mask.include(view=slc))
             if not mask.any():
@@ -717,9 +718,9 @@ def build_covariance(spectrum=None,
     if index is None:
         index = np.arange(len(spectrum))
     if channel_correlation is None:
-        return((rms[:, np.newaxis])**2)
+        return(np.diag(rms**2))
     if len(channel_correlation) == 1:
-        return((rms[:, np.newaxis])**2)
+        return(np.diag(rms**2))
     distance = np.abs(index[:, np.newaxis] - index[np.newaxis, :])
     covar = rms[:, np.newaxis] * rms[np.newaxis, :]
     maxdist = len(channel_correlation)
@@ -730,3 +731,128 @@ def build_covariance(spectrum=None,
 
 def calculate_channel_correlation(cube, length=1):
     raise NotImplementedError
+
+
+def write_moment1_advanced(cube,
+                           broad_mask=None,
+                           moment1_prior=None,
+                           velocity_convention='radio',
+                           order='bilinear',
+                           outfile=None,
+                           errorfile=None,
+                           rms=None,
+                           channel_correlation=None,
+                           overwrite=True,
+                           vfield_reject_thresh=30 * u.km / u.s,
+                           mom0_thresh_for_mom1=2.0,
+                           unit=None):
+    """
+    Writes a moment 1 map 
+    
+    Parameters:
+    -----------
+    
+    cube : SpectralCube
+        SpectralCube of original data with strict masking applied
+    
+    Keywords:
+    ---------
+    
+    broad_mask : SpectralCube or np.array
+        Array with same shape as the input SpectralCube to be used 
+        as the broad (permissive) mask
+        
+    moment1_prior : FITS filename or Projection
+        FITS filename or Projection containting the velocity field prior
+    
+    """
+
+    mom1strict = cube.moment1()
+    if unit is not None:
+        mom1strict = mom1strict.to(unit)
+        spaxis = cube.spectral_axis.to(unit).value
+    else:
+        spaxis = cube.spectral_axis.value
+
+    if moment1_prior is not None:
+        if type(moment1_prior) is Projection:
+            mom1prior = moment1_prior
+        elif type(moment1_prior) is str:
+            hdu_list = fits.open(moment1_prior)
+            mom1prior = Projection.from_hdu(hdulist[0])
+        if unit is not None:
+            mom1prior.to(unit)
+        mom1prior = mom1prior.reproject(mom1strict.header, order=order)
+    else:
+        mom1prior = None
+
+    
+    if type(broad_mask) is SpectralCube:
+        broad_cube = cube.with_mask(
+            broad_mask.filled_data[:].value.astype(np.bool),
+            inherit_mask=False)
+    elif type(broad_mask) is np.ndarray:
+        broad_cube = cube.with_mask(broad_mask.astype(np.bool),
+                                    inherit_mask=False)
+        
+    mom0broad = broad_cube.moment0()
+    mom1broad = broad_cube.moment1()
+    mom0err = None
+    mom1err = None
+    # Calculate errors in mom0broad, mom1broad
+    if rms is not None and errorfile is not None:
+        if channel_correlation is None:
+            channel_correlation = np.array([1])
+
+        mom0err = np.empty(mom0broad.shape)
+        mom0err.fill(np.nan)
+        mom1err = np.empty(mom1broad.shape)
+        mom1err.fill(np.nan)
+
+        for x, y, slc in cube._iter_rays(0):
+            mask = np.squeeze(cube._mask.include(view=slc))
+            if not mask.any():
+                continue
+            index = np.where(mask)[0]
+            rms_spec = rms.flattened(slc).value
+            spec = cube.flattened(slc).value
+
+            covar = build_covariance(spectrum=spec,
+                                     rms=rms_spec,
+                                     channel_correlation=channel_correlation,
+                                     index=index)
+            vval = spaxis[index]
+            sum_T = np.sum(spec)
+            sum_vT = np.sum(spec * vval)
+
+            jacobian = vval / sum_T - sum_vT / sum_T**2
+            mom1err[x, y] = np.dot(
+                np.dot(jacobian[np.newaxis, :], covar),
+                jacobian[:, np.newaxis])**0.5
+            mom0err = (np.sum(covar**2))**0.5
+        mom0err = u.Quantity(mom0err, mom0broad.unit, copy=False)
+        mom1err = u.Quantity(mom1err, mom1broad.unit, copy=False)
+        if unit is not None:
+            mom1err = mom1err.to(unit)
+    
+    mom1hybrid = mom1strict.value
+    valid_broad_mom1 = np.isfinite(mom1broad.value)
+    valid_broad_mom1[np.isfinite(mom1strict)] = False
+    
+    if mom0err is not None:
+        valid_broad_mom1 = (mom0.value 
+                            > (mom0_thresh_for_mom1 * mom0err.value))
+    if mom1prior is not None:
+        valid_broad_mom1 = (valid_broad_mom1 * 
+                            (np.abs(mom1broad - mom1prior) 
+                             < vfield_reject_thresh)
+                            )
+    mom1hybrid[valid_broad_mom1] = (mom1broad.value)[valid_broad_mom1]
+    return(mom1hybrid)
+        # mom0err_proj = Projection(mom0err,
+        #                           wcs=mom0_broad.wcs,
+        #                           header=mom0_broad.header)
+        # mom1err_proj = Projection(mom1err,
+        #                           wcs=mom1_broad.wcs,
+        #                           header=mom1_broad.header)
+        
