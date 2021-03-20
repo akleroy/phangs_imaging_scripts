9b854a98811392930168e72ba9dee13cb8a50c09
1054 <>
2021-03-20 15:30:21 +0100

    Fixed fname_dict issue in task_singlescale_mask in handlerImaging.

--- phangsPipeline/handlerImaging.py
+++ phangsPipeline/handlerImaging.py
@@ -628,38 +628,67 @@ class ImagingHandler(handlerTemplate.HandlerTemplate):
         mask for deep cleaning. Used before running a deep single
         scale clean.
         """
-                
-        imagename = clean_call.get_param('imagename')+'.image'
+        
+        if product is None:
+            logger.error("Require a product. Returning.")
+            raise Exception("Require a product. Returning.")
+            return()
+        
+        # Get fname dict
+        fname_dict = self._fname_dict(product = product, imagename = clean_call.get_param('imagename'))
+        
+        # get imagename
+        imagename = fname_dict['image']
         if not os.path.isdir(imagename):
             logger.error("Image not found: "+imagename)
             return()
-
+        
+        # print message
         logger.info("")
         logger.info("&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%")
         logger.info("Creating signal-to-noise based clean mask for:")
-        logger.info(str(clean_call.get_param('imagename')))
+        logger.info(str(imagename))
         logger.info("&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%")
         logger.info("")
-        
+            
         if self._dry_run:
             return()
         if not casa_enabled:
             return()
+        
+        # check if line product
+        is_line_product = product in self._kh.get_line_products()
+        
+        if is_line_product:
 
-        # Get fname dict
-        fname_dict = self._fname_dict(product = product, imagename = clean_call.get_param('imagename'))
+            # signal_mask
+            msr.signal_mask(cube_root=fname_dict['root'],
+                            out_file=fname_dict['mask'],
+                            suffix_in=fname_dict['suffix'], 
+                            suffix_out=fname_dict['suffix'], 
+                            operation='AND',
+                            high_snr=4.0,
+                            low_snr=2.0,
+                            absolute=False)
 
-        # signal_mask
-        msr.signal_mask(cube_root=fname_dict['root'],
-                        out_file=fname_dict['mask'],
-                        suffix_in=fname_dict['suffix'], 
-                        suffix_out=fname_dict['suffix'], 
-                        operation='AND',
-                        high_snr=4.0,
-                        low_snr=2.0,
-                        absolute=False)
+            clean_call.set_param('usemask','user')
+            
+        else:
+            
+            # for continuum product, we need to make 2D mask
 
-        clean_call.set_param('usemask','user')
+            # signal_mask
+            msr.signal_mask(cube_root=fname_dict['root'],
+                            out_file=fname_dict['mask'],
+                            suffix_in=fname_dict['suffix'], 
+                            suffix_out=fname_dict['suffix'], 
+                            operation='AND',
+                            high_snr=4.0,
+                            low_snr=2.0,
+                            absolute=False, 
+                            do_roll=False)
+
+            clean_call.set_param('usemask','user')
 
         return()
     
